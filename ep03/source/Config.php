f0d37fcf6dab023fa4d2cd93551d7acc6a88bd03
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-09-21 18:22:59 -0300

    php mailer

---
+++ ep03/source/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+const MAIL = [
+   "host" => "",
+   "port" => "",
+   "user" => "",
+   "passwd" => "",
+   "from_name" => "Paschoal Colombini",
+   "from_email" => "paschoal.colombini@live.com"
+];
